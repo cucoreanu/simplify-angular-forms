ba8399ef8377b61a64b79606ba436cffbed17fc7
Nicolae Cucoreanu <nicolae@cucorea.nu>
2023-12-07 10:31:53 +0200

    bugfix: add tabindex to checkboxes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import {provideRouter} from "@angular/router";
 import {BigFormComponent} from "./big-form/big-form.component";
-import {TemplateDrivenComponent} from "./template-driven-vs-reactive/template-driven/template-driven.component";
-import {TemplateDrivenVsReactiveComponent} from "./template-driven-vs-reactive/template-driven-vs-reactive.component";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,21 +8,6 @@ export const appConfig: ApplicationConfig = {
       {
         path: '',
         component: BigFormComponent
-      },
-      {
-        path: 'template-driven-vs-reactive',
-        component: TemplateDrivenVsReactiveComponent,
-        children: [
-          {
-            path: '',
-            redirectTo: 'template-driven',
-            pathMatch: 'full'
-          },
-          {
-            path: 'template-driven',
-            component: TemplateDrivenComponent
-          }
-        ]
       }
     ])
   ]
